cc9997b9662e8783fcb7c7d682a746147e6e1897
Edward Jiang <ed@padmapper.com>
2015-10-03 13:04:06 -0700

    Initial Commit

---
+++ HackJamDemo 2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HackJamDemo 2
+//
+//  Created by Edward Jiang on 10/3/15.
+//  Copyright © 2015 Edward Jiang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
